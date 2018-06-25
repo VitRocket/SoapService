6bfb153ff6c9d5d6c7053450c16203d59d3745a9
VitRocket <vitrocket@gmail.com>
2018-06-25 10:19:34 +0300

    soap get product from db

--- src/main/java/com/example/soap/ProductEndpoint.java
+++ src/main/java/com/example/soap/ProductEndpoint.java
@@ -1,24 +1,34 @@
 package com.example.soap;
 
+import com.example.dao.domain.Product;
+import com.example.dao.service.ProductService;
 import com.example.soap.product.ProductModel;
 import com.example.soap.product.ProductRequest;
 import com.example.soap.product.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+@Slf4j
 @Endpoint
+@RequiredArgsConstructor
 public class ProductEndpoint {
+
+    private final ProductService productService;
 
     @PayloadRoot(namespace = "http://example.com/soap/product", localPart = "ProductRequest")
     @ResponsePayload
     public ProductResponse getProduct(@RequestPayload ProductRequest request) {
 
+        Product product = productService.getProductById(request.getId());
+        log.info(product.toString());
         ProductModel productModel = new ProductModel();
-        productModel.setId(request.getId());
-        productModel.setName("Product1");
-        productModel.setDescription("My new product");
+        productModel.setId(product.getId());
+        productModel.setName(product.getName());
+        productModel.setDescription(product.getDescription());
 
         ProductResponse response = new ProductResponse();
         response.setProduct(productModel);
